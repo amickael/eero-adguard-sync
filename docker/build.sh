e431056ad1dff3a05e90fc4f9eebdd9c1af6de0b
Andrew Mickael <andrew.mickael@gmail.com>
2022-01-10 15:44:00 -0600

    bump ver

--- docker/build.sh
+++ docker/build.sh
@@ -2,5 +2,5 @@ VERSION=$(cat ../VERSION)
 TAG="$DOCKER_HUB_USERNAME/eero-adguard-sync:$VERSION"
 
 echo "$DOCKER_HUB_TOKEN" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
-docker build -t "$TAG" --build-arg EAG_VERSION="$VERSION" .
+docker build --no-cache -t "$TAG" --build-arg EAG_VERSION="$VERSION" .
 docker push "$TAG"
--- docker/sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-eag-sync sync \
-  --eero-cookie "$EAG_EERO_COOKIE" \
-  --adguard-host "$EAG_ADGUARD_HOST" \
-  --adguard-user "$EAG_ADGUARD_USER" \
-  --adguard-password "$EAG_ADGUARD_PASS" \
-  "-y$EAG_SYNC_FLAGS"
