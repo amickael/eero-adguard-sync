df7d51d6478cff31eb6f712756c02e12cd760435
Andrew Mickael <andrew.mickael@gmail.com>
2022-01-10 12:14:22 -0600

    fix dupes

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,6 @@
-TAG="amickael/eero-adguard-sync:$(cat ../VERSION)"
+VERSION=$(cat ../VERSION)
+TAG="amickael/eero-adguard-sync:$VERSION"
 
 echo "$DOCKER_HUB_TOKEN" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
-docker build -t "$TAG" .
+docker build -t "$TAG" --build-arg EAG_VERSION="$VERSION" .
 docker push "$TAG"
